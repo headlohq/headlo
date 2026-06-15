d1986ad48b4a7db1ef770d980f610fa2785c4511
Phil @headlo <support@headlo.com>
2026-06-14 22:06:14 -0700

    Release headlo-sdk

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+export { createClient }           from './client'
+export { verifyComponentCode }    from './verify'
+export type { HeadloClient } from './client'
+export type {
+  HeadloErrorCode,
+  HeadloResult,
+  CollectionRecord,
+  CollectionListResponse,
+  CollectionRecordResponse,
+  PageModulesResponse,
+  ModuleResponse,
+  SingleModuleResponse,
+  SitePage,
+  SitePagesResponse,
+  ListOptions,
+  CollectionFilter,
+  FilterOp,
+  SiteCreatedResponse,
+  ComponentRow,
+  TokenClaims,
+  ComponentUtils,
+  PropComponentDef,
+  PropComponentApp,
+  PropComponentResponse,
+  PropServiceDef,
+  PropServiceResponse,
+  PropServiceClient,
+} from './types'
